9a49bf18986c3cea5ce6d7f70c5140a628cc8cc0
ERivasG <erivga18@gmail.com>
2016-06-07 14:03:58 -0300

    configuracion base de datos

---
+++ application/views/elements/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+        <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.12.4.min.js"); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+    </head>
+    <body>
+        
+                
+    </body>
+</html>
